526803b9255b5f51a41c3a3b3922de85b442e993
Delisa Mason <iskanamagus@gmail.com>
2014-07-09 18:35:01 -0700

    Expose generic function for parsing a date into Sense BLE data format

--- Pod/Classes/BLE/SENDateUtils.h
+++ Pod/Classes/BLE/SENDateUtils.h
@@ -4,3 +4,5 @@
 NSData* SEN_dataForCurrentDate();
 
 NSDate* SEN_dateForData(NSData* data);
+
+NSData* SEN_dataForDate(NSDate* date);
--- Pod/Classes/BLE/SENDateUtils.m
+++ Pod/Classes/BLE/SENDateUtils.m
@@ -11,9 +11,8 @@ struct SENDateBytes {
     u_int8_t weekday;
 };
 
-NSData* SEN_dataForCurrentDate()
+NSData* SEN_dataForDate(NSDate* date)
 {
-    NSDate* date = [NSDate date];
     NSCalendar* calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
     NSDateComponents* components = [calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit | NSWeekdayCalendarUnit)fromDate:date];
     struct SENDateBytes dateBytes;
@@ -26,6 +25,11 @@ NSData* SEN_dataForCurrentDate()
     dateBytes.second = components.second;
     dateBytes.weekday = weekday == 1 ? 7 : weekday - 1;
     return [NSData dataWithBytes:&dateBytes length:sizeof(struct SENDateBytes)];
+}
+
+NSData* SEN_dataForCurrentDate()
+{
+    return SEN_dataForDate([NSDate date]);
 }
 
 NSDate* SEN_dateForData(NSData* data)
