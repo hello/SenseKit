0432b60e1465e1c667aca1b476c66e3deb556ef7
Delisa Mason <iskanamagus@gmail.com>
2014-07-29 18:24:41 -0700

    Add endpoints for fetching historical sensor data

--- Pod/Classes/API/SENAPIRoom.h
+++ Pod/Classes/API/SENAPIRoom.h
@@ -13,4 +13,27 @@ typedef void (^SENAPIDataBlock)(id data, NSError* error);
  *  @param completion block invoked when the network call is completed asynchronously
  */
 + (void)currentWithCompletion:(SENAPIDataBlock)completion;
+
+/**
+ *  GET /room/:sensorName/day?from=:date
+ *
+ *  Fetch historical values for a given sensor
+ *
+ *  @param sensorName name of the sensor for data
+ *  @param completion block invoked when the network call is completed asynchronously
+ */
++ (void)hourlyHistoricalDataForSensorWithName:(NSString*)sensorName
+                                   completion:(SENAPIDataBlock)completion;
+
+/**
+ *  GET /room/:sensorName/week?from=:date
+ *
+ *  Fetch historical values for a given sensor
+ *
+ *  @param sensorName name of the sensor for data
+ *  @param completion block invoked when the network call is completed asynchronously
+ */
++ (void)dailyHistoricalDataForSensorWithName:(NSString*)sensorName
+                                  completion:(SENAPIDataBlock)completion;
+
 @end
--- Pod/Classes/API/SENAPIRoom.m
+++ Pod/Classes/API/SENAPIRoom.m
@@ -8,11 +8,48 @@
 
 + (void)currentWithCompletion:(SENAPIDataBlock)completion
 {
-    [[SENAPIClient HTTPSessionManager] GET:@"/room/current" parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+    [[SENAPIClient HTTPSessionManager] GET:@"/room/current" parameters:nil success:^(NSURLSessionDataTask* task, id responseObject) {
         completion(responseObject, nil);
-    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+    } failure:^(NSURLSessionDataTask* task, NSError* error) {
         completion(nil, error);
     }];
+}
+
++ (void)hourlyHistoricalDataForSensorWithName:(NSString*)sensorName
+                                   completion:(SENAPIDataBlock)completion
+{
+    [self historicalDataForSensorWithName:sensorName timeScope:@"day" completion:completion];
+}
+
++ (void)dailyHistoricalDataForSensorWithName:(NSString*)sensorName completion:(SENAPIDataBlock)completion
+{
+    [self historicalDataForSensorWithName:sensorName timeScope:@"week" completion:completion];
+}
+
++ (void)historicalDataForSensorWithName:(NSString*)sensorName
+                              timeScope:(NSString*)scope
+                             completion:(SENAPIDataBlock)completion
+{
+    NSString* timestamp = [[self UTCMillisecondTimestampDateFormatter] stringFromDate:[NSDate date]];
+    NSString* path = [NSString stringWithFormat:@"/room/%@/%@", sensorName, scope];
+    [[SENAPIClient HTTPSessionManager] GET:path parameters:@{ @"from" : timestamp } success:^(NSURLSessionDataTask* task, id responseObject) {
+        completion(responseObject, nil);
+    } failure:^(NSURLSessionDataTask* task, NSError* error) {
+        completion(nil, error);
+    }];
+}
+
++ (NSDateFormatter*)UTCMillisecondTimestampDateFormatter
+{
+    static NSDateFormatter* dateFormatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        NSTimeZone *tz = [NSTimeZone timeZoneForSecondsFromGMT:0];
+        [dateFormatter setDateFormat:@"A"];
+        [dateFormatter setTimeZone:tz];
+    });
+    return dateFormatter;
 }
 
 @end
