f1c984c1e7f9e4aec91229b173112255f69985c8
Delisa Mason <iskanamagus@gmail.com>
2014-08-06 10:44:36 -0700

    Implement background noise persistence

--- Pod/Classes/Model/SENBackgroundNoise.h
+++ Pod/Classes/Model/SENBackgroundNoise.h
@@ -3,6 +3,11 @@
 
 @interface SENBackgroundNoise : NSObject <NSCoding>
 
++ (instancetype)savedBackgroundNoise;
+
+- (instancetype)initWithSoundName:(NSString*)soundName;
+- (void)save;
+- (BOOL)isOn;
+
 @property (nonatomic, strong) NSString* soundName;
-@property (nonatomic, getter=isOn) BOOL on;
 @end
--- Pod/Classes/Model/SENBackgroundNoise.m
+++ Pod/Classes/Model/SENBackgroundNoise.m
@@ -1,24 +1,50 @@
 
 #import "SENBackgroundNoise.h"
+#import "SENKeyedArchiver.h"
 
 NSString* const SENBackgroundNoiseSoundNameKey = @"sound_name";
-NSString* const SENBackgroundNoiseOnKey = @"on";
+NSString* const SENBackgroundNoiseStorageKey = @"SENBackgroundNoise";
 
 @implementation SENBackgroundNoise
 
-- (id)initWithCoder:(NSCoder *)aDecoder
++ (instancetype)savedBackgroundNoise
+{
+    SENBackgroundNoise* noise = [[[SENKeyedArchiver objectsForKey:SENBackgroundNoiseStorageKey] allObjects] firstObject];
+    if (!noise) {
+        noise = [[SENBackgroundNoise alloc] initWithSoundName:NSLocalizedString(@"noise.sound-name.none", nil)];
+    }
+    return noise;
+}
+
+- (instancetype)initWithSoundName:(NSString*)soundName
 {
     if (self = [super init]) {
-        _on = [[aDecoder decodeObjectForKey:SENBackgroundNoiseOnKey] boolValue];
+        _soundName = soundName;
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder*)aDecoder
+{
+    if (self = [super init]) {
         _soundName = [aDecoder decodeObjectForKey:SENBackgroundNoiseSoundNameKey];
     }
     return self;
 }
 
-- (void)encodeWithCoder:(NSCoder *)aCoder
+- (void)encodeWithCoder:(NSCoder*)aCoder
 {
-    [aCoder encodeObject:@(self.on) forKey:SENBackgroundNoiseOnKey];
     [aCoder encodeObject:self.soundName forKey:SENBackgroundNoiseSoundNameKey];
+}
+
+- (void)save
+{
+    [SENKeyedArchiver setObjects:[NSSet setWithObject:self] forKey:SENBackgroundNoiseStorageKey];
+}
+
+- (BOOL)isOn
+{
+    return ![self.soundName isEqualToString:NSLocalizedString(@"noise.sound-name.none", nil)];
 }
 
 @end
