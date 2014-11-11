ec3b1a6975e8d59de696f2e90e8214b199a7cc9e
Delisa Mason <iskanamagus@gmail.com>
2014-11-10 17:24:06 -0800

    Add sound property to segment

--- Example/Tests/SENSleepResultSegmentSpec.m
+++ Example/Tests/SENSleepResultSegmentSpec.m
@@ -6,15 +6,23 @@ SPEC_BEGIN(SENSleepResultSegmentSpec)
 
 describe(@"SENSleepResultSegment", ^{
 
+    __block SENSleepResultSegment* segment = nil;
+
+    afterEach(^{
+        segment = nil;
+    });
+
     describe(@"initialization", ^{
-        __block SENSleepResultSegment* segment = nil;
         NSTimeInterval timeInterval = [[NSDate date] timeIntervalSince1970];
         NSDictionary* json = @{@"id": @94841103,
                                @"duration":@18181000,
                                @"timestamp":@(timeInterval * 1000),
                                @"message": @"there's a disturbance in the force",
-                               @"event_type": @"light",
-                               @"sleep_depth": @2};
+                               @"event_type": @"sound",
+                               @"sleep_depth": @2,
+                               @"sound": @{
+                                       @"url":@"http://example.com/sound.caf",
+                                       @"duration_millis":@53484}};
 
         beforeEach(^{
             segment = [[SENSleepResultSegment alloc] initWithDictionary:json];
@@ -43,16 +51,26 @@ describe(@"SENSleepResultSegment", ^{
         it(@"sets the sleep depth", ^{
             [[@([segment sleepDepth]) should] equal:json[@"sleep_depth"]];
         });
+
+        it(@"sets the sound url", ^{
+            [[[segment.sound URLPath] should] equal:json[@"sound"][@"url"]];
+        });
+
+        it(@"sets the sound duration", ^{
+            [[@([segment.sound durationMillis]) should] equal:json[@"sound"][@"duration_millis"]];
+        });
     });
 
     describe(@"serialization", ^{
-        __block SENSleepResultSegment* segment = nil;
         __block SENSleepResultSegment* serializedSegment = nil;
         NSTimeInterval timeInterval = [[NSDate date] timeIntervalSince1970] - 10000;
         NSDictionary* json = @{@"id": @448917,
                                @"message": @"there may have been an earthquake",
                                @"event_type": @"noise",
-                               @"date": @(timeInterval * 1000)};
+                               @"date": @(timeInterval * 1000),
+                               @"sound": @{
+                                       @"url":@"http://example.com/sound.mkv",
+                                       @"duration_millis":@53112}};
 
         beforeEach(^{
             segment = [[SENSleepResultSegment alloc] initWithDictionary:json];
@@ -83,10 +101,17 @@ describe(@"SENSleepResultSegment", ^{
         it(@"has the same sleep depth after serialization", ^{
             [[@(serializedSegment.sleepDepth) should] equal:@(segment.sleepDepth)];
         });
+
+        it(@"has the same sound URL after serialization", ^{
+            [[serializedSegment.sound.URLPath should] equal:segment.sound.URLPath];
+        });
+
+        it(@"has the same sound URL after serialization", ^{
+            [[@(serializedSegment.sound.durationMillis) should] equal:@(segment.sound.durationMillis)];
+        });
     });
 
     describe(@"updating", ^{
-        __block SENSleepResultSegment* segment = nil;
         NSDictionary* json = @{@"id": @9422198, @"duration": @553000};
         NSDictionary* updatedJSON = @{@"duration": @125500};
 
