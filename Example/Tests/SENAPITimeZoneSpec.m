d15d87bd820fc0aad0bbabf1a10ececd9899de4c
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-03-17 16:31:37 -0700

    opening additional method to accept NSTimeZone object in API wrapper

--- Example/Tests/SENAPITimeZoneSpec.m
+++ Example/Tests/SENAPITimeZoneSpec.m
@@ -14,25 +14,59 @@ SPEC_BEGIN(SENAPITimeZoneSpec)
 
 describe(@"SENAPITimeZone", ^{
     
-    describe(@"+ setCurrentTimeZone", ^{
+    beforeAll(^{
+        [[LSNocilla sharedInstance] start];
+    });
+    
+    afterAll(^{
+        [[LSNocilla sharedInstance] stop];
+    });
+    
+    describe(@"+ setTimeZone:completion", ^{
         
-        beforeAll(^{
-            [[LSNocilla sharedInstance] start];
+        it(@"should make a POST call", ^{
+            [[SENAPIClient should] receive:@selector(POST:parameters:completion:)];
+            [SENAPITimeZone setTimeZone:[NSTimeZone localTimeZone] completion:nil];
         });
         
-        afterAll(^{
-            [[LSNocilla sharedInstance] stop];
-        });
-
-        beforeEach(^{
+        it(@"should call completion block", ^{
             [SENAPIClient stub:@selector(POST:parameters:completion:) withBlock:^id(NSArray *params) {
                 SENAPIDataBlock block = [params lastObject];
                 block(nil, nil);
                 return nil;
             }];
+            
+            __block BOOL calledBack = NO;
+            [SENAPITimeZone setTimeZone:[NSTimeZone localTimeZone] completion:^(id data, NSError *error) {
+                calledBack = YES;
+            }];
+            [[@(calledBack) should] beYes];
+        });
+        
+        it(@"should complete with error", ^{
+            __block NSError* timezoneError = nil;
+            [SENAPITimeZone setTimeZone:nil completion:^(id data, NSError *error) {
+                timezoneError = error;
+            }];
+            [[timezoneError should] beNonNil];
+        });
+        
+    });
+    
+    describe(@"+ setCurrentTimeZone", ^{
+
+        it(@"should call setTimeZone:completion", ^{
+            [[SENAPITimeZone should] receive:@selector(setTimeZone:completion:)];
+            [SENAPITimeZone setCurrentTimeZone:nil];
         });
         
         it(@"should call completion block", ^{
+            [SENAPITimeZone stub:@selector(setTimeZone:completion:) withBlock:^id(NSArray *params) {
+                SENAPIDataBlock block = [params lastObject];
+                block(nil, nil);
+                return nil;
+            }];
+            
             __block BOOL calledback = NO;
             [SENAPITimeZone setCurrentTimeZone:^(id data, NSError *error) {
                 calledback = YES;
@@ -42,6 +76,7 @@ describe(@"SENAPITimeZone", ^{
         });
         
     });
+    
 });
 
 SPEC_END
