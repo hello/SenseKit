1c72c56164dbeade538c921b51c7f66e71d4413e
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-04-16 17:03:19 -0700

    take first sleep time and last wake up time for HK

--- Example/Tests/SENServiceHealthKitSpec.m
+++ Example/Tests/SENServiceHealthKitSpec.m
@@ -8,11 +8,14 @@
 
 #import <Kiwi/Kiwi.h>
 #import <HealthKit/HealthKit.h>
+#import "SENSleepResult.h"
 #import "SENServiceHealthKit.h"
 
 @interface SENServiceHealthKit()
 
 @property (nonatomic, strong) HKHealthStore* hkStore;
+
+- (NSArray*)sleepDataPointsForSleepResult:(SENSleepResult*)sleepResult;
 
 @end
 
@@ -86,6 +89,136 @@ describe(@"SENServiceHealthKitSpec", ^{
             [[@(canWrite) should] beYes];
             
         });
+        
+    });
+    
+    describe(@"-sleepDataPointsForSleepResult:", ^{
+        
+        __block SENServiceHealthKit* service = nil;
+        
+        beforeEach(^{
+            service = [SENServiceHealthKit sharedService];
+        });
+        
+        context(@"has no sleep or wake up events", ^{
+            
+            __block SENSleepResult* sleepResult = nil;
+            
+            beforeEach(^{
+                
+                NSDictionary* json = @{@"score": @100,
+                                       @"date":@([[NSDate date] timeIntervalSince1970] * 1000),
+                                       @"segments" : @[
+                                               @{@"id": @33421},
+                                               @{@"id": @32995},
+                                               ],
+                                       @"statistics" : @{
+                                               @"total_sleep" : @330,
+                                               @"sound_sleep" : @123,
+                                               @"times_awake" : @1,
+                                               }
+                                       };
+                
+                sleepResult = [[SENSleepResult alloc] initWithDictionary:json];
+                
+            });
+            
+            it(@"should return no data points", ^{
+                
+                NSArray* dataPoints = [service sleepDataPointsForSleepResult:sleepResult];
+                [[dataPoints shouldNot] beNil];
+                [[dataPoints should] haveCountOf:0];
+                
+            });
+            
+        });
+        
+        context(@"has sleep event, but no wake event", ^{
+            
+            __block SENSleepResult* sleepResult = nil;
+            
+            beforeEach(^{
+                
+                NSDictionary* json = @{@"score": @100,
+                                       @"date":@([[NSDate date] timeIntervalSince1970] * 1000),
+                                       @"segments" : @[
+                                               @{@"id": @33421},
+                                               @{@"id": @32995,
+                                                 @"event_type" : @"SLEEP",
+                                                 @"timestamp" : @([[NSDate date] timeIntervalSince1970] * 1000)
+                                                 }
+                                               ],
+                                       @"statistics" : @{
+                                               @"total_sleep" : @330,
+                                               @"sound_sleep" : @123,
+                                               @"times_awake" : @1,
+                                               }
+                                       };
+                
+                sleepResult = [[SENSleepResult alloc] initWithDictionary:json];
+                
+            });
+            
+            it(@"should return no data points", ^{
+                
+                NSArray* dataPoints = [service sleepDataPointsForSleepResult:sleepResult];
+                [[dataPoints shouldNot] beNil];
+                [[dataPoints should] haveCountOf:0];
+                
+            });
+            
+        });
+        
+        context(@"has sleep and wake events", ^{
+            
+            __block SENSleepResult* sleepResult = nil;
+            __block NSTimeInterval sleepTime = [[NSDate date] timeIntervalSince1970];
+            __block NSTimeInterval wakeTime = [[NSDate date] timeIntervalSince1970];
+            
+            beforeEach(^{
+                
+                NSDictionary* json = @{@"score": @100,
+                                       @"date":@([[NSDate date] timeIntervalSince1970] * 1000),
+                                       @"segments" : @[
+                                               @{@"id": @32995,
+                                                 @"event_type" : @"SLEEP",
+                                                 @"timestamp" : @(sleepTime * 1000)
+                                                 },
+                                               @{@"id" : @32996,
+                                                 @"event_type" : @"WAKE_UP",
+                                                 @"timestamp" : @([[NSDate date] timeIntervalSince1970] * 1000)
+                                                 },
+                                               @{@"id" : @32997,
+                                                 @"event_type" : @"WAKE_UP",
+                                                 @"timestamp" : @(wakeTime * 1000)
+                                                 }
+                                               ],
+                                       @"statistics" : @{
+                                               @"total_sleep" : @330,
+                                               @"sound_sleep" : @123,
+                                               @"times_awake" : @1,
+                                               }
+                                       };
+                
+                sleepResult = [[SENSleepResult alloc] initWithDictionary:json];
+                
+            });
+            
+            it(@"should return 1 data point", ^{
+                
+                NSArray* dataPoints = [service sleepDataPointsForSleepResult:sleepResult];
+                [[dataPoints should] haveCountOf:1];
+                
+                NSDate* startDate = [NSDate dateWithTimeIntervalSince1970:sleepTime];
+                NSDate* endDate = [NSDate dateWithTimeIntervalSince1970:wakeTime];
+                HKCategorySample* sample = dataPoints[0];
+                [[[sample startDate] should] equal:startDate];
+                [[[sample endDate] should] equal:endDate];
+                
+            });
+            
+        });
+        
         
     });
     
