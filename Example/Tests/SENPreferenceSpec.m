59a900d6d81ab617569bdaa7a84241f034c9888e
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-08-20 13:02:57 -0700

    create new method to initialize preference with default

--- Example/Tests/SENPreferenceSpec.m
+++ Example/Tests/SENPreferenceSpec.m
@@ -129,6 +129,113 @@ describe(@"SENPreference", ^{
         
     });
     
+    describe(@"-initWithType:", ^{
+        
+        __block SENPreference *timePreference = nil;
+        __block SENPreference *tempPreference = nil;
+        __block SENPreference *weightPreference = nil;
+        __block SENPreference *heightPreference = nil;
+        __block SENPreference *audioPreference = nil;
+        __block SENPreference *pushConditionsPreference = nil;
+        __block SENPreference *pushScorePreference = nil;
+        
+        context(@"US locale", ^{
+            
+            beforeAll(^{
+                NSLocale* locale = [NSLocale localeWithLocaleIdentifier:@"en_US"];
+                [NSLocale stub:@selector(currentLocale) andReturn:locale];
+                
+                timePreference = [[SENPreference alloc] initWithType:SENPreferenceTypeTime24];
+                tempPreference = [[SENPreference alloc] initWithType:SENPreferenceTypeTempCelcius];
+                weightPreference = [[SENPreference alloc] initWithType:SENPreferenceTypeWeightMetric];
+                heightPreference = [[SENPreference alloc] initWithType:SENPreferenceTypeHeightMetric];
+                audioPreference = [[SENPreference alloc] initWithType:SENPreferenceTypeEnhancedAudio];
+                pushConditionsPreference = [[SENPreference alloc] initWithType:SENPreferenceTypePushConditions];
+                pushScorePreference = [[SENPreference alloc] initWithType:SENPreferenceTypePushScore];
+            });
+            
+            afterAll(^{
+                [NSLocale clearStubs];
+            });
+                                  
+            it(@"time preference is 12hr, not enabled for military time", ^{
+                [[@([timePreference isEnabled]) should] beNo];
+            });
+            
+            it(@"temp preference is not celcius", ^{
+                [[@([tempPreference isEnabled]) should] beNo];
+            });
+            
+            it(@"weight preference is not using metric system", ^{
+                [[@([weightPreference isEnabled]) should] beNo];
+            });
+            
+            it(@"height preference is not using metric system", ^{
+                [[@([heightPreference isEnabled]) should] beNo];
+            });
+            
+            it(@"audio preference is disabled", ^{
+                [[@([audioPreference isEnabled]) should] beNo];
+            });
+            
+            it(@"push condition preference is enabled", ^{
+                [[@([pushConditionsPreference isEnabled]) should] beYes];
+            });
+            
+            it(@"push score preference is enabled", ^{
+                [[@([pushScorePreference isEnabled]) should] beYes];
+            });
+        });
+        
+        context(@"German locale, non US", ^{
+            
+            beforeAll(^{
+                NSLocale* locale = [NSLocale localeWithLocaleIdentifier:@"de_EU"];
+                [NSLocale stub:@selector(currentLocale) andReturn:locale];
+                
+                timePreference = [[SENPreference alloc] initWithType:SENPreferenceTypeTime24];
+                tempPreference = [[SENPreference alloc] initWithType:SENPreferenceTypeTempCelcius];
+                weightPreference = [[SENPreference alloc] initWithType:SENPreferenceTypeWeightMetric];
+                heightPreference = [[SENPreference alloc] initWithType:SENPreferenceTypeHeightMetric];
+                audioPreference = [[SENPreference alloc] initWithType:SENPreferenceTypeEnhancedAudio];
+                pushConditionsPreference = [[SENPreference alloc] initWithType:SENPreferenceTypePushConditions];
+                pushScorePreference = [[SENPreference alloc] initWithType:SENPreferenceTypePushScore];
+            });
+            
+            afterAll(^{
+                [NSLocale clearStubs];
+            });
+            
+            it(@"time preference is 24hr", ^{
+                [[@([timePreference isEnabled]) should] beYes];
+            });
+            
+            it(@"temp preference is celcius", ^{
+                [[@([tempPreference isEnabled]) should] beYes];
+            });
+            
+            it(@"weight preference is using metric system", ^{
+                [[@([weightPreference isEnabled]) should] beYes];
+            });
+            
+            it(@"height preference is using metric system", ^{
+                [[@([heightPreference isEnabled]) should] beYes];
+            });
+            
+            it(@"audio preference is disabled", ^{
+                [[@([audioPreference isEnabled]) should] beNo];
+            });
+            
+            it(@"push condition preference is enabled", ^{
+                [[@([pushConditionsPreference isEnabled]) should] beYes];
+            });
+            
+            it(@"push score preference is enabled", ^{
+                [[@([pushScorePreference isEnabled]) should] beYes];
+            });
+        });
+        
+    });
     
     describe(@"-dictionaryValue", ^{
         
