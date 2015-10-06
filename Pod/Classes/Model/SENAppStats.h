9a00066ca15f84cadc2ee529713c318725ad9a51
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-10-06 10:21:16 -0700

    add comments

--- Pod/Classes/Model/SENAppStats.h
+++ Pod/Classes/Model/SENAppStats.h
@@ -10,9 +10,27 @@
 
 @interface SENAppStats : NSObject
 
+/**
+ * @property lastViewedInsights
+ *
+ * @discussion
+ * The date in which insights were last viewed.  It may be null if account has
+ * yet to update this property before
+ */
 @property (nonatomic, strong, nullable) NSDate* lastViewedInsights;
 
+/**
+ * Initialize the object with the provied raw dictionary values retrieved from
+ * an API request
+ *
+ * @param dictionary: dictionary containing raw values retrieved from API
+ */
 - (nonnull instancetype)initWithDictionary:(nonnull NSDictionary*)dictionary;
+
+/**
+ * @return a raw dictionary containing the values encapsulated in this object 
+ *         that can be returned as the body of a request to the API
+ */
 - (nonnull NSDictionary*)dictionaryValue;
 
 @end
