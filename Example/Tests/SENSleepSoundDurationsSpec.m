30f40d906459807e553b87b47f826311cee19fbf
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-03-24 12:20:36 -0700

    model tests

---
+++ Example/Tests/SENSleepSoundDurationsSpec.m
@@ -0,0 +1,46 @@
+//
+//  SENSleepSoundDurationsSpec.m
+//  SenseKit
+//
+//  Created by Jimmy Lu on 3/24/16.
+//  Copyright © 2016 Hello, Inc. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "Model.h"
+
+SPEC_BEGIN(SENSleepSoundDurationsSpec)
+
+describe(@"SENSleepSoundDurations", ^{
+    
+    describe(@"-initWithDictionary:", ^{
+        
+        __block SENSleepSoundDurations* durations = nil;
+        __block NSDictionary* rawData = nil;
+        
+        beforeEach(^{
+            rawData = @{@"durations" : @[@{@"id" : @1,
+                                           @"name" : @"long time"}]};
+            durations = [[SENSleepSoundDurations alloc] initWithDictionary:rawData];
+        });
+        
+        afterEach(^{
+            rawData = nil;
+            durations = nil;
+        });
+        
+        it(@"should contain 1 duration", ^{
+            [[@([[durations durations] count]) should] equal:@1];
+        });
+        
+        it(@"should contain a sound with id, name, and preview url", ^{
+            SENSleepSoundDuration* duration = [[durations durations] firstObject];
+            [[[duration identifier] should] equal:@1];
+            [[[duration localizedName] should] equal:@"long time"];
+        });
+        
+    });
+    
+});
+
+SPEC_END
