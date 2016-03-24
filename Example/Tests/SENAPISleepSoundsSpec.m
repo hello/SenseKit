fa6fd1629754eeb5d5d9e12cf20d4f0898e31ade
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-03-23 17:11:27 -0700

    sleep durations

--- Example/Tests/SENAPISleepSoundsSpec.m
+++ Example/Tests/SENAPISleepSoundsSpec.m
@@ -133,6 +133,125 @@ describe(@"SENAPISleepSounds", ^{
         
     });
     
+    describe(@"+availableDurations:", ^{
+        
+        context(@"server decided to return an array of durations", ^{
+            
+            __block SENSleepSoundDuration* durations = nil;
+            __block NSError* apiError = nil;
+            __block BOOL calledBack = NO;
+            
+            beforeEach(^{
+                [SENAPIClient stub:@selector(GET:parameters:completion:) withBlock:^id(NSArray *params) {
+                    SENAPIDataBlock cb = [params lastObject];
+                    cb (@[], nil);
+                    return nil;
+                }];
+                
+                [SENAPISleepSounds availableDurations:^(id data, NSError *error) {
+                    durations = data;
+                    apiError = error;
+                    calledBack = YES;
+                }];
+            });
+            
+            afterEach(^{
+                [SENAPIClient clearStubs];
+                durations = nil;
+                apiError = nil;
+                calledBack = NO;
+            });
+            
+            it(@"should not return any data back", ^{
+                [[durations should] beNil];
+            });
+            
+            it(@"should not return any errors back", ^{
+                [[apiError should] beNil];
+            });
+            
+            it(@"should have called back", ^{
+                [[@(calledBack) should] beYes];
+            });
+            
+        });
+        
+        context(@"server returned a dictionary for data", ^{
+            
+            __block SENSleepSoundDurations* durations = nil;
+            __block NSError* apiError = nil;
+            
+            beforeEach(^{
+                [SENAPIClient stub:@selector(GET:parameters:completion:) withBlock:^id(NSArray *params) {
+                    SENAPIDataBlock cb = [params lastObject];
+                    cb (@{}, nil);
+                    return nil;
+                }];
+                
+                [SENAPISleepSounds availableDurations:^(id data, NSError *error) {
+                    durations = data;
+                    apiError = error;
+                }];
+            });
+            
+            afterEach(^{
+                [SENAPIClient clearStubs];
+                durations = nil;
+                apiError = nil;
+            });
+            
+            it(@"should return a sleep sound duration object", ^{
+                [[durations should] beKindOfClass:[SENSleepSoundDurations class]];
+            });
+            
+            it(@"should not return any errors back", ^{
+                [[apiError should] beNil];
+            });
+            
+            it(@"should not contain any data in the sleep durations object", ^{
+                [[[durations durations] should] beEmpty];
+            });
+            
+        });
+        
+        context(@"server returned an error", ^{
+            
+            __block SENSleepSoundDurations* durations = nil;
+            __block NSError* apiError = nil;
+            
+            beforeEach(^{
+                [SENAPIClient stub:@selector(GET:parameters:completion:) withBlock:^id(NSArray *params) {
+                    SENAPIDataBlock cb = [params lastObject];
+                    cb (nil, [NSError errorWithDomain:@"t"
+                                                 code:-1
+                                             userInfo:nil]);
+                    return nil;
+                }];
+                
+                [SENAPISleepSounds availableDurations:^(id data, NSError *error) {
+                    durations = data;
+                    apiError = error;
+                }];
+            });
+            
+            afterEach(^{
+                [SENAPIClient clearStubs];
+                durations = nil;
+                apiError = nil;
+            });
+            
+            it(@"should not return a sleep sound durations object", ^{
+                [[durations should] beNil];
+            });
+            
+            it(@"should return an error", ^{
+                [[apiError should] beNonNil];
+            });
+            
+        });
+        
+    });
+    
 });
 
 SPEC_END
