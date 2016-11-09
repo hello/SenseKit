29545d6cd578a0503a9e8bd20513a2c037dfdf23
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-11-08 16:44:09 -0800

    wrapper around alerts API + test case

---
+++ Example/Tests/SENAPISystemAlertsSpec.m
@@ -0,0 +1,66 @@
+//
+//  SENAPISystemAlertsSpec.m
+//  SenseKit
+//
+//  Created by Jimmy Lu on 11/8/16.
+//  Copyright © 2016 Hello, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <SenseKit/Model.h>
+#import <SenseKit/API.h>
+
+SPEC_BEGIN(SENAPISystemAlertsSpec)
+
+describe(@"SENAPISystemAlerts", ^{
+    
+    describe(@"+getSystemAlerts:", ^{
+        
+        context(@"api returns 1 expansion unreachable alert", ^{
+            
+            __block id returnedObj = nil;
+            __block NSError* apiError = nil;
+            
+            beforeEach(^{
+                [SENAPIClient stub:@selector(GET:parameters:completion:) withBlock:^id(NSArray *params) {
+                    SENAPIDataBlock cb = [params  lastObject];
+                    cb (@[@{@"title" : @"title",
+                            @"body" : @"body",
+                            @"category" : @"EXPANSION_UNREACHABLE"}], nil);
+                    return nil;
+                }];
+                
+                [SENAPISystemAlerts getSystemAlerts:^(id data, NSError *error) {
+                    returnedObj = data;
+                    apiError = error;
+                }];
+                
+            });
+            
+            afterEach(^{
+                [SENAPIClient clearStubs];
+                returnedObj = nil;
+                apiError = nil;
+            });
+            
+            it(@"should return an array of 1 alert object", ^{
+                [[returnedObj should] beKindOfClass:[NSArray class]];
+                [[returnedObj should] haveCountOf:1];
+                
+                SENSystemAlert* alert = [returnedObj firstObject];
+                [[[alert localizedTitle] should] beNonNil];
+                [[[alert localizedBody] should] beNonNil];
+            });
+            
+            it(@"should not have an error", ^{
+                [[apiError should] beNil];
+            });
+            
+        });
+        
+    });
+    
+});
+
+SPEC_END
